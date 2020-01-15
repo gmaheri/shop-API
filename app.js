06645774c787ba2f03506c4a59ea5d76647c3327
Gregory <gregory.maheri@gmail.com>
2020-01-15 12:20:51 +0300

    added error handling

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan')
 
 const routerProducts = require('./api/routes/products')
 const routerOrder = require('./api/routes/orders')
 
 const port = process.env.PORT || 3000;
+app.use(morgan('dev'));
 
+//Routes that handle requests
 app.use('/products', routerProducts);
 app.use('/orders', routerOrder);
+
+//error handling
+app.use((req, res, next) => {
+  const error = new Error('Not Found');
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error : {
+      message : error.message
+    }
+  });
+});
 
 app.listen(port, ()=> {
   console.log(`Server is up and running on port: ${port}`)
